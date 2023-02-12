757b99f57d42484ae7a6abb0276e09c5d8d3d7c2
Mohammad Jubair <you@example.com>
2023-02-13 02:26:36 +0600

    Fourth Push

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,19 +5,29 @@ let withdrawBtn = document.getElementById("withdraw-btn");
 //step 2 : add a eventLitener with Called id
 withdrawBtn.addEventListener("click", () => {
   let newgetValueWithDrawField = parseFloat(withdrawField.value);
+  if (isNaN(newgetValueWithDrawField)) {
+    alert("Please Provide a valid Number");
+    return;
+  }
 
   //step 3 : Select a Withdraw amount
   let withdrawTotalAmount = document.getElementById("withdraw-total");
   let pregetWithdrawFinalVal = parseFloat(withdrawTotalAmount.innerText);
 
+  //step 5 : select a total balance from main balance
+  let totalBalance = document.getElementById("total-balance");
+  let preTotalBalance = parseFloat(totalBalance.innerText);
+
+  //Balance check for condition
+  if (newgetValueWithDrawField > preTotalBalance) {
+    alert("Don't Withdraw, You Unsuffiecient Balance !");
+    return;
+  }
+
   //step 4 : calcute total number
   let currentWithdrawalTotal =
     newgetValueWithDrawField + pregetWithdrawFinalVal;
   withdrawTotalAmount.innerText = currentWithdrawalTotal;
-
-  //step 5 : select a total balance from main balance
-  let totalBalance = document.getElementById("total-balance");
-  let preTotalBalance = parseFloat(totalBalance.innerText);
 
   let nowTheTotalBalance = preTotalBalance - newgetValueWithDrawField;
 
